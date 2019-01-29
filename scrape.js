6bd49a405477925260e323ce907a898752757fdf
Yusaku Kasahara <mark.kasahara@gmail.com>
2019-01-29 20:43:24 +0900

    scrape file made

--- scrape.js
+++ scrape.js
@@ -5,6 +5,11 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://yelp.com');
   await page.screenshot({path: 'example.png'});
+  const h2 = await page.evaluate(
+    () => document.querySelector("h2").textContent
+  );
+  console.log(h2);
 
+  
   await browser.close();
 })();
